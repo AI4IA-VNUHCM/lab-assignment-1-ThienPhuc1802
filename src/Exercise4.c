67f237a75a7d3b9273b4e80ef6c75c003891e433
ThienPhuc1802 <91868450+ThienPhuc1802@users.noreply.github.com>
2021-10-04 22:09:34 +0700

    ex 4

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -24,6 +24,36 @@ int main(int argc, char *argv[]) {
 	int height = atoi(argv[1]);
 	int border = atoi(argv[2]);
 	//Your codes here
+int i,t,j,k,b;
+for (i=1;i<=height;++i){
 	
+       for (t=1;t<=height-i;++t){
+           printf("  ");
+	   }
+	   if (i==3 || i==4 || i==5){
+		   for (j=1;j<=2*i-1;++j){ 
+			   if (i==3 && j==3){
+				   printf("  ");
+				   continue;
+			   }
+			   if(i==4 && (j==3 || j==4 || j==5)){
+				   printf("  ");
+				   continue;
+			   }
+			   if (i==5 &&(j==3 || j==4 || j==5 || j==6 || j==7 )){
+				   printf("  ");
+				   continue;
+			   }
+		printf("* ");
+		}
+		printf("\n");
+		continue;
+	   }
+	for (j=1;j<=2*i-1;++j){ 
+		printf("* ");
+		}
+		printf("\n");
+}
+
 	return 0;
 }
